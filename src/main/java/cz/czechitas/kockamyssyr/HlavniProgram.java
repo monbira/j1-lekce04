83252d64493f1bffe353b2a712e1b90b1cfe06b3
Filip Jirsák <filip@jirsak.org>
2023-02-28 12:36:51 +0100

    Zadání pro jaro 2023

--- src/main/java/cz/czechitas/kockamyssyr/HlavniProgram.java
+++ src/main/java/cz/czechitas/kockamyssyr/HlavniProgram.java
@@ -3,11 +3,20 @@ package cz.czechitas.kockamyssyr;
 import cz.czechitas.kockamyssyr.api.*;
 
 import java.awt.*;
+import java.util.Random;
 
 /**
  * Hlaví třída pro hru Kočka–myš–sýr.
  */
 public class HlavniProgram {
+    private final Random random = new Random();
+
+    private final int VELIKOST_PRVKU = 50;
+    private final int SIRKA_OKNA = 1000 - VELIKOST_PRVKU;
+    private final int VYSKA_OKNA = 600 - VELIKOST_PRVKU;
+
+    private Cat tom;
+    private Mouse jerry;
 
     /**
      * Spouštěcí metoda celé aplikace.
@@ -22,21 +31,49 @@ public class HlavniProgram {
      * Hlavní metoda obsahující výkonný kód.
      */
     public void run() {
-        new Tree(new Point(400, 200));
-        new Tree(new Point(400, 200));
-        new Tree(new Point(400, 200));
-        new Tree(new Point(400, 200));
-        // TODO: Sem vepište svůj program
-
-        Cat tom = new Cat(new Point(100, 100));
+        Cat tom = vytvorKocku();
         tom.setBrain(new KeyboardBrain());
 
-        new Cheese(new Point(200, 200));
-
-        Mouse jerry = new Mouse(new Point(600, 200));
+        Mouse jerry = vytvorMys();
         jerry.setBrain(new KeyboardBrain(KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D));
 
-        new Meat(new Point(200, 500));
+        vytvorVeci(4);
+        chytMys();
+    }
+
+    public void chytMys() {
+        // TODO: Sem vepište svůj program
+    }
+
+    public void vytvorVeci(int pocetStromu) {
+        for (int i = 0; i < pocetStromu; i++) {
+            vytvorStrom();
+        }
+        vytvorSyr();
+        vytvorJitrnici();
+    }
+    public Tree vytvorStrom() {
+        return new Tree(vytvorNahodnyBod());
+    }
+
+    public Cat vytvorKocku() {
+        return new Cat(vytvorNahodnyBod());
+    }
+
+    public Mouse vytvorMys() {
+        return new Mouse(vytvorNahodnyBod());
+    }
+
+    public Cheese vytvorSyr() {
+        return new Cheese(vytvorNahodnyBod());
+    }
+
+    public Meat vytvorJitrnici() {
+        return new Meat(vytvorNahodnyBod());
+    }
+
+    private Point vytvorNahodnyBod() {
+        return new Point(random.nextInt(SIRKA_OKNA), random.nextInt(VYSKA_OKNA));
     }
 
 }
